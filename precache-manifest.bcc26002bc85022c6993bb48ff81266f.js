30733fef06fd8ff1f045fd00570ee1ea12575579
Andrew <susanto.andrew@ymail.com>
2019-11-29 06:34:20 +0700

    initial commit

---
+++ precache-manifest.bcc26002bc85022c6993bb48ff81266f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98feb57f27eb11441c287e262e8e7c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b90bfbd30693277d235",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c150dd3566285eb68a08",
+    "url": "/static/js/2.e59f7bf6.chunk.js"
+  },
+  {
+    "revision": "1b90bfbd30693277d235",
+    "url": "/static/js/main.21352ebe.chunk.js"
+  },
+  {
+    "revision": "9f59143be9121defdeba",
+    "url": "/static/js/runtime-main.4466c8c2.js"
+  }
+]);
